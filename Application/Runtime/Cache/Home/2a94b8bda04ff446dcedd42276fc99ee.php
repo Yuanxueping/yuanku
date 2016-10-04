1628a1169e34877aff5234bee40af4d61bc3c671
LaoLeo <1074699327@qq.com>
2016-10-04 17:41:17 +0800

    更新了首页

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,220 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+<head lang="zh">
+	<title><?php echo ($title); ?></title>
+
+	<meta charset="UTF-8">
+
+	 
+	 <link rel="stylesheet" href="/201610/thinkphp_3.2.3_full/yuanku/Public/bootstrap-3.3.0/css/bootstrap.min.css">
+	 <link rel="stylesheet" href="/201610/thinkphp_3.2.3_full/yuanku/Public/css/home/headAndFoot.css">
+	 <script src="/201610/thinkphp_3.2.3_full/yuanku/Public/js/jquery.1.11.1.min.js">	</script>
+	 <script src="/201610/thinkphp_3.2.3_full/yuanku/Public/bootstrap-3.3.0/js/bootstrap.min.js">	</script>
+
+	<!--  <link rel="stylesheet" href="css/headAndFoot.css" />
+	 <link rel="stylesheet" href="css/bootstrap.min.css" /> -->
+		
+	<!--  <script type="text/javascript" src="js/jquery-1.9.1.min.js" ></script> 
+	 <script type="text/javascript" src="js/bootstrap.min.js" ></script>-->
+
+	<!--HOME css-->
+	<link rel="stylesheet" href="/201610/thinkphp_3.2.3_full/yuanku/Public/css/home/home.css" />
+	
+</head>
+<body>
+
+	<nav class="navbar navbar-default top-nav" >
+		  <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header"> 
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <!--<a class="navbar-brand" href="#">
+		      	<img class="logo" src="img/logo.png" />
+		      </a>-->
+		    </div>		
+		    
+			    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				      <ul class="nav navbar-nav navbar-right color">
+				        <li><a href="#">首页</a></li>
+				        <li><a href="#">登录</a></li>
+				        <li><a href="#">注册</a></li>
+				        <li><a href="#">新闻</a></li>
+				        <li><a href="#">联系我们</a></li>
+				        <li><a href="#">关于我们</a></li>
+				        <li>
+				        	<a  data-toggle="modal" data-target="#exampleModal" data-whatever="@mdo" style="cursor: pointer;">
+				        		<span class="glyphicon glyphicon-search" style="color: white;" ></span>
+				        	</a>
+				        </li>
+				      </ul>		          			        				        	
+	        </div>
+		  </div>
+		</nav>
+
+		<div style="height:300px"></div>
+
+
+<div class="views-element-container feature_section3">
+
+	<div class="section_title1_1">
+		<h2 class="caps white">Portfolio Works </h2>
+		<div class="linebg"></div>
+		<p class="fontdefaulf">There are variations available majoritaey suffered alteration words which look believable <br /> dolor sit amet consectetuer adipiscing elit suspendisse justdo.</p>
+	</div>
+	
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-6">
+				<a href="/sites/default/files/2016-05/project1_1.jpg" class="cbp-caption cbp-lightbox" data-title="Color  by M.A Anderson">
+					<div class="cbp-caption-defaultWrap">
+						<img src="/201610/thinkphp_3.2.3_full/yuanku/Public/img/highstand homepage/project1_1.jpg" class="img-responsive" alt="pro" typeof="Image" />
+					</div>
+					<div class="cbp-caption-activeWrap">
+						<div class="cbp-l-caption-alignLeft">
+							<div class="cbp-l-caption-body">
+								<div class="cbp-l-caption-title">Color</div>
+								<div class="cbp-l-caption-desc">by M.A Anderson</div>
+							</div>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-6">
+				<div class="row">
+					<div class="col-md-6">
+						<a href="https://vimeo.com/161689529" class="cbp-caption cbp-lightbox" data-title="Fledglings  by M.A Anderson">
+							<div class="cbp-caption-defaultWrap">
+								<img src="/201610/thinkphp_3.2.3_full/yuanku/Public/img/highstand homepage/04-big.jpg" class="img-responsive" alt="" typeof="Image" />
+							</div>
+							<div class="cbp-caption-activeWrap">
+								<div class="cbp-l-caption-alignLeft">
+									<div class="cbp-l-caption-body">
+										<div class="cbp-l-caption-title">Fledglings</div>
+										<div class="cbp-l-caption-desc">by M.A Anderson</div>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-6">
+						<a href="/sites/default/files/2016-05/08-big.jpg" class="cbp-caption cbp-lightbox" data-title="Packages  by M.A Anderson">
+							<div class="cbp-caption-defaultWrap">
+								<img src="/201610/thinkphp_3.2.3_full/yuanku/Public/img/highstand homepage/08-big.jpg" class="img-responsive" alt="Portfolio" typeof="Image" />
+							</div>
+							<div class="cbp-caption-activeWrap">
+								<div class="cbp-l-caption-alignLeft">
+									<div class="cbp-l-caption-body">
+										<div class="cbp-l-caption-title">Packages</div>
+										<div class="cbp-l-caption-desc">by M.A Anderson</div>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="col-md-6">
+						<a href="/sites/default/files/2016-05/07-big.jpg" class="cbp-caption cbp-lightbox" data-title="LG dLite  by M.A Anderson">
+							<div class="cbp-caption-defaultWrap">
+								<img src="/201610/thinkphp_3.2.3_full/yuanku/Public/img/highstand homepage/07-big.jpg" class="img-responsive"  alt="Portfolio" typeof="Image" />
+							</div>
+							<div class="cbp-caption-activeWrap">
+								<div class="cbp-l-caption-alignLeft">
+									<div class="cbp-l-caption-body">
+										<div class="cbp-l-caption-title">LG dLite</div>
+										<div class="cbp-l-caption-desc">by M.A Anderson</div>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+					<div class="col-md-6">
+						<a href="/sites/default/files/2016-05/05-big.jpg" class="cbp-caption cbp-lightbox" data-title="LG dLite  by M.A Anderson">
+							<div class="cbp-caption-defaultWrap">
+								<img src="/201610/thinkphp_3.2.3_full/yuanku/Public/img/highstand homepage/05-big.jpg" class="img-responsive" alt="Portfolio" typeof="Image" />
+							</div>
+							<div class="cbp-caption-activeWrap">
+								<div class="cbp-l-caption-alignLeft">
+									<div class="cbp-l-caption-body">
+										<div class="cbp-l-caption-title">LG dLite</div>
+										<div class="cbp-l-caption-desc">by M.A Anderson</div>
+									</div>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				
+			</div><!--col-md-6-->
+		</div>
+		
+		<div class="row">
+			<div class="col-md-3">
+				<a href="/sites/default/files/2016-05/04-big.jpg" class="cbp-caption cbp-lightbox" data-title="LG dLite  by M.A Anderson">
+					<div class="cbp-caption-defaultWrap">
+						<img src="/201610/thinkphp_3.2.3_full/yuanku/Public/img/highstand homepage/161689529.jpg" class="img-responsive" alt="Portfolio" typeof="Image" />
+					</div>
+					<div class="cbp-caption-activeWrap">
+						<div class="cbp-l-caption-alignLeft">
+							<div class="cbp-l-caption-body">
+								<div class="cbp-l-caption-title">LG dLite</div>
+								<div class="cbp-l-caption-desc">by M.A Anderson</div>
+							</div>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-3">
+				<a href="https://vimeo.com/14368986" class="cbp-caption cbp-lightbox" data-title="LG dLite  by M.A Anderson">
+					<div class="cbp-caption-defaultWrap">
+						<img src="/201610/thinkphp_3.2.3_full/yuanku/Public/img/highstand homepage/14368986.jpg" class="img-responsive" alt="" typeof="Image" />
+					</div>
+					<div class="cbp-caption-activeWrap">
+						<div class="cbp-l-caption-alignLeft">
+							<div class="cbp-l-caption-body">
+								<div class="cbp-l-caption-title">LG dLite</div>
+								<div class="cbp-l-caption-desc">by M.A Anderson</div>
+							</div>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-3">
+				<a href="https://www.youtube.com/watch?v=FkTybqcX-Yo" class="cbp-caption cbp-lightbox" data-title="Civil War - Trailer 1  by M.A Anderson">
+					<div class="cbp-caption-defaultWrap">
+						<img src="/201610/thinkphp_3.2.3_full/yuanku/Public/img/highstand homepage/FkTybqcX-Yo.jpg" class="img-responsive" alt="" typeof="Image" />
+					</div>
+					<div class="cbp-caption-activeWrap">
+						<div class="cbp-l-caption-alignLeft">
+							<div class="cbp-l-caption-body">
+								<div class="cbp-l-caption-title">Civil War - Trailer 1</div>
+								<div class="cbp-l-caption-desc">by M.A Anderson</div>
+							</div>
+						</div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-3">
+				<a href="https://www.youtube.com/watch?v=dKrVegVI0Us" class="cbp-caption cbp-lightbox" data-title="Civil War - Trailer 2  by M.A Anderson">
+					<div class="cbp-caption-defaultWrap">
+						<img src="/201610/thinkphp_3.2.3_full/yuanku/Public/img/highstand homepage/dKrVegVI0Us.jpg" class="img-responsive" alt="" typeof="Image" />
+					</div>
+					<div class="cbp-caption-activeWrap">
+						<div class="cbp-l-caption-alignLeft">
+							<div class="cbp-l-caption-body">
+								<div class="cbp-l-caption-title">Civil War - Trailer 2</div>
+								<div class="cbp-l-caption-desc">by M.A Anderson</div>
+							</div>
+						</div>
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
