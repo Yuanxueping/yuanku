2914ee7eb28c816e04b634b88c52e4c2f36591c1
smith <3162547163@qq.com>
2016-10-13 09:45:14 +0800

    登录页面

--- wx_sample.php
+++ wx_sample.php
@@ -79,7 +79,6 @@ elseif ($weixin_msg_a['MsgType']=='event') {
        # 两个人？的菜单" 
      }
   }
-
   else{
       if ($weixin_msg_a['Event']=='subscribe') {//订阅
        // FromUserName 请求获取用户信息的接口
@@ -463,3 +462,33 @@ class wechatCallbackapiTest
 }
 
 ?>
+
+<script type="text/javascript">
+  
+  function onBridgeReady(){
+   WeixinJSBridge.invoke(
+       'getBrandWCPayRequest', {
+           "appId" ： "wx2421b1c4370ec43b",     //公众号名称，由商户传入     
+           "timeStamp"：" 1395712654",         //时间戳，自1970年以来的秒数     
+           "nonceStr" ： "e61463f8efa94090b1f366cccfbbb444", //随机串     
+           "package" ： "prepay_id=u802345jgfjsdfgsdg888",     
+           "signType" ： "MD5",         //微信签名方式：     
+           "paySign" ： "70EA570631E4BB79628FBCA90534C63FF7FADD89" //微信签名 
+       },
+       function(res){     
+           if(res.err_msg == "get_brand_wcpay_request：ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。 
+       }
+   ); 
+}
+if (typeof WeixinJSBridge == "undefined"){
+   if( document.addEventListener ){
+       document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+   }else if (document.attachEvent){
+       document.attachEvent('WeixinJSBridgeReady', onBridgeReady); 
+       document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+   }
+}else{
+   onBridgeReady();
+} 
+
+</script>
