7518429e719fc1b1089ac0e2ef8f2e73ccdd7dbe
yangliu <710831265@qq.com>
2016-10-20 16:07:39 +0800

    后台新闻列表

--- Application/Admin/Controller/NewsController.class.php
+++ Application/Admin/Controller/NewsController.class.php
@@ -5,6 +5,12 @@ use Common\Controller\AuthController;
 
 class NewsController extends AuthController {
     public function index(){
+    	$news_m = M('News');
+		
+		$news_list = $news_m -> alias('n') -> field('n.id as nid,title,name,sort,content,img,date') -> join('author') -> where('author_id=author.id') -> select();
+		
+		$this -> assign('news_list', $news_list);
+		
     	$this->display();
     }
 
