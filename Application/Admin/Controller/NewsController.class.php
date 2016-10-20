434dae1a18900c1c5f015169e7aa97c469409097
yangliu <710831265@qq.com>
2016-10-20 16:28:32 +0800

    新增news——sort表，修改news表的字段、数据

--- Application/Admin/Controller/NewsController.class.php
+++ Application/Admin/Controller/NewsController.class.php
@@ -7,7 +7,11 @@ class NewsController extends AuthController {
     public function index(){
     	$news_m = M('News');
 		
-		$news_list = $news_m -> alias('n') -> field('n.id as nid,title,name,sort,content,img,date') -> join('author') -> where('author_id=author.id') -> select();
+		$news_list = $news_m -> alias('n')
+							 -> field('n.id as nid,title,name,sort_name,content,img,date')
+							 -> join('author ON author_id=author.id')
+							 -> join('news_sort ON sort_id=news_sort.id')
+							 -> select();
 		
 		$this -> assign('news_list', $news_list);
 		
