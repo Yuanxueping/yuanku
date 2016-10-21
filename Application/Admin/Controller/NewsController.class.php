0d1596d35b86772d6eafc1d69ec377c2b7bc4fd3
dihaosam <511186470@qq.com>
2016-10-21 14:19:25 +0800

    newsort

--- Application/Admin/Controller/NewsController.class.php
+++ Application/Admin/Controller/NewsController.class.php
@@ -22,8 +22,59 @@ class NewsController extends AuthController {
     {
     	$this->display();
     }
-   public function newsort(){
-     	$this->display(); 
+  
+    public function newsort(){
+   	    $news_m = M('News');
+		
+		$news_list = $news_m -> alias('n')
+							 -> field('n.id as nid,title,name,sort_name,content,img,date')
+							 -> join('author ON author_id=author.id')
+							 -> join('news_sort ON sort_id=news_sort.id')
+							 -> order('sort_name')
+							 -> select();
+		
+		
+		$this -> assign('news_list', $news_list);
+//		echo $news_m->getLastSql();
+//		exit();
+		
+    	$this->display(); 
+
+   }
+    public function sortbytime(){
+   	    $news_m = M('News');
+		
+		$news_list = $news_m -> alias('n')
+							 -> field('n.id as nid,title,name,sort_name,content,img,date')
+							 -> join('author ON author_id=author.id')
+							 -> join('news_sort ON sort_id=news_sort.id')
+							 -> order('date')
+							 -> select();
+		
+		
+		$this -> assign('news_list', $news_list);
+//		echo $news_m->getLastSql();
+//		exit();
+		
+    	$this->display(); 
+
+   }
+    public function sortbyauthor(){
+   	    $news_m = M('News');
+		
+		$news_list = $news_m -> alias('n')
+							 -> field('n.id as nid,title,name,sort_name,content,img,date')
+							 -> join('author ON author_id=author.id')
+							 -> join('news_sort ON sort_id=news_sort.id')
+							 -> order('author.id')
+							 -> select();
+		
+		
+		$this -> assign('news_list', $news_list);
+//		echo $news_m->getLastSql();
+//		exit();
+		
+    	$this->display(); 
 
    }
 }
