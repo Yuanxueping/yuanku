2e9160507f5298e0192a94daadcd138cdbe97dc5
dihaosam <511186470@qq.com>
2016-10-24 17:53:04 +0800

    change

--- Application/Runtime/Cache/Admin/b595b808c2ebeeeb0478ffc30bb4e87a.php
+++ Application/Runtime/Cache/Admin/b595b808c2ebeeeb0478ffc30bb4e87a.php
@@ -19,22 +19,22 @@
   <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
  
   
-  <link rel="stylesheet" href="/yuanku/Public/css/admin/material-design-iconic-font.css">
-  <link rel="stylesheet" href="/yuanku/Public/css/admin/animate.css">
-  <link rel="stylesheet" href="/yuanku/Public/css/admin/metisMenu.css">
-  <link rel="stylesheet" href="/yuanku/Public/css/admin/jquery_002.css">
-  <link rel="stylesheet" href="/yuanku/Public/css/admin/waves.css">
-  <link rel="stylesheet" href="/yuanku/Public/css/admin/toastr.css">
+  <link rel="stylesheet" href="/lidong/yuanku/Public/css/admin/material-design-iconic-font.css">
+  <link rel="stylesheet" href="/lidong/yuanku/Public/css/admin/animate.css">
+  <link rel="stylesheet" href="/lidong/yuanku/Public/css/admin/metisMenu.css">
+  <link rel="stylesheet" href="/lidong/yuanku/Public/css/admin/jquery_002.css">
+  <link rel="stylesheet" href="/lidong/yuanku/Public/css/admin/waves.css">
+  <link rel="stylesheet" href="/lidong/yuanku/Public/css/admin/toastr.css">
 
 
 
-  <link rel="stylesheet" href="/yuanku/Public/css/admin/jquery.css">
+  <link rel="stylesheet" href="/lidong/yuanku/Public/css/admin/jquery.css">
 
-  <link rel="stylesheet" href="/yuanku/Public/css/admin/style.css">
-  <link rel="stylesheet" href="/yuanku/Public/css/admin/bootstrap.css">
-  <link rel="stylesheet" href="/yuanku/Public/css/admin/common.css">
+  <link rel="stylesheet" href="/lidong/yuanku/Public/css/admin/style.css">
+  <link rel="stylesheet" href="/lidong/yuanku/Public/css/admin/bootstrap.css">
+  <link rel="stylesheet" href="/lidong/yuanku/Public/css/admin/common.css">
 
-  <script src="/yuanku/Public/js/jquery.js"></script>
+  <script src="/lidong/yuanku/Public/js/jquery.js"></script>
 <!--
   <link rel="icon" href="img/favicon.ico" type="image/x-icon" />
   <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
@@ -43,7 +43,7 @@
       <script src="bower_components/html5shiv/dist/html5shiv.min,js"></script>
       <script src="bower_components/respondJs/dest/respond.min.js"></script>
     <![endif]-->
-<script src="/yuanku/Public/js/jquery_008.js"></script></head>
+<script src="/lidong/yuanku/Public/js/jquery_008.js"></script></head>
 
 <body class="fixed-all boxed-layout fixed-sidebar mCustomScrollbar _mCS_4 mCS-autoHide" style="overflow: visible;">
  <div id="mCSB_4" class="mCustomScrollBox mCS-minimal-dark mCSB_vertical mCSB_outside" style="max-height: none;" tabindex="0"><div id="mCSB_4_container" class="mCSB_container" style="position: relative; top: -164px; left: 0px;" dir="ltr">
@@ -57,7 +57,7 @@
    <nav class="navbar navbar-indigo">
  <div class="navbar-header container brand-orange">
    <a href="#" class="menu-toggle"><i class="zmdi zmdi-menu"></i></a>
-   <a href="/yuanku/admin" class="logo">力动企业</a>
+   <a href="/lidong/yuanku/admin" class="logo">力动企业</a>
    <a href="http://91.234.35.26/pacificonis-admin/v1.0.1/land/l3/index.html" class="icon-logo"></a>
  </div>
   <div class="navbar-container clearfix">
@@ -122,7 +122,7 @@
     <li class="profile-sidebar-container">
       <div class="profile-sidebar text-center">
         <div class="profile-userpic">
-          <img src="/yuanku/Uploads<?php echo ($user_info["head_photo"]); ?>" class="img-responsive img-circle center-block mCS_img_loaded" alt="user">
+          <img src="/lidong/yuanku/Uploads<?php echo ($user_info["head_photo"]); ?>" class="img-responsive img-circle center-block mCS_img_loaded" alt="user">
           <span class="online"></span>
         </div>
         <div class="profile-usertitle">
@@ -375,7 +375,7 @@
  <div class="row">
         <div class="col-lg-3 col-md-6">
           <div class="content-box p-20 twitter white f-s-16">
-            <img src="/yuanku/Public/images/admin/avatar.png" class="pull-left m-r-10 mCS_img_loaded" alt="avatar">
+            <img src="/lidong/yuanku/Public/images/admin/avatar.png" class="pull-left m-r-10 mCS_img_loaded" alt="avatar">
             <span class="text-uppercase">第一组</span>  <br>
             <span class="half-opacity">@组长：黄寰</span>
             <span class="zmdi zmdi-twitter p-absolute t-20 r-20 f-s-20"></span>
@@ -388,7 +388,7 @@
         </div>
          <div class="col-lg-3 col-md-6">
           <div class="content-box p-20 soundcloud white f-s-16">
-            <img src="/yuanku/Public/images/admin/avatar4.png" class="pull-left m-r-10 mCS_img_loaded" alt="avatar">
+            <img src="/lidong/yuanku/Public/images/admin/avatar4.png" class="pull-left m-r-10 mCS_img_loaded" alt="avatar">
             <span class="text-uppercase">第二组</span>  <br>
             <span class="half-opacity">@组长：沈迪豪</span>
             <span class="zmdi zmdi-soundcloud p-absolute t-20 r-20 f-s-20"></span>
@@ -401,7 +401,7 @@
         </div>
         <div class="col-lg-3 col-md-6">
             <div class="content-box p-20 facebook white f-s-16">
-              <img src="/yuanku/Public/images/admin/avatar2.png" class="pull-left m-r-10 mCS_img_loaded" alt="avatar">
+              <img src="/lidong/yuanku/Public/images/admin/avatar2.png" class="pull-left m-r-10 mCS_img_loaded" alt="avatar">
               <span class="text-uppercase">第三组</span>  <br>
               <span class="half-opacity">@组长：袁雪萍</span>
               <span class="zmdi zmdi-facebook p-absolute t-20 r-20 f-s-20"></span>
@@ -414,7 +414,7 @@
         </div>
         <div class="col-lg-3 col-md-6">
           <div class="content-box p-20 gplus white f-s-16">
-            <img src="/yuanku/Public/images/admin/avatar3.png" class="pull-left m-r-10 mCS_img_loaded" alt="avatar">
+            <img src="/lidong/yuanku/Public/images/admin/avatar3.png" class="pull-left m-r-10 mCS_img_loaded" alt="avatar">
             <span class="text-uppercase">第四组</span>  <br>
             <span class="half-opacity">@smith</span>
             <span class="zmdi zmdi-google-plus p-absolute t-20 r-20 f-s-20"></span>
@@ -437,22 +437,22 @@
 
    </div>
 </div></div>
-  <script src="/yuanku/Public/js/bootstrap.js"></script>
-  <script src="/yuanku/Public/js/metisMenu.js"></script>
-  <script src="/yuanku/Public/js/jquery_006.js"></script>
-  <script src="/yuanku/Public/js/waves.js"></script>
-  <script src="/yuanku/Public/js/toastr.js"></script>
+  <script src="/lidong/yuanku/Public/js/bootstrap.js"></script>
+  <script src="/lidong/yuanku/Public/js/metisMenu.js"></script>
+  <script src="/lidong/yuanku/Public/js/jquery_006.js"></script>
+  <script src="/lidong/yuanku/Public/js/waves.js"></script>
+  <script src="/lidong/yuanku/Public/js/toastr.js"></script>
 
 
-  <script src="/yuanku/Public/js/jquery_004.js"></script>
-  <script src="/yuanku/Public/js/dataTables.js"></script>
-  <script src="/yuanku/Public/js/moment.js"></script>
+  <script src="/lidong/yuanku/Public/js/jquery_004.js"></script>
+  <script src="/lidong/yuanku/Public/js/dataTables.js"></script>
+  <script src="/lidong/yuanku/Public/js/moment.js"></script>
 
-  <script src="/yuanku/Public/js/Chart.js"></script>
-  <script src="/yuanku/Public/js/jquery_005.js"></script>
-  <script src="/yuanku/Public/js/jquery_007.js"></script>
-  <script src="/yuanku/Public/js/jquery_003.js"></script>
-  <script src="/yuanku/Public/js/jquery_002.js"></script>
+  <script src="/lidong/yuanku/Public/js/Chart.js"></script>
+  <script src="/lidong/yuanku/Public/js/jquery_005.js"></script>
+  <script src="/lidong/yuanku/Public/js/jquery_007.js"></script>
+  <script src="/lidong/yuanku/Public/js/jquery_003.js"></script>
+  <script src="/lidong/yuanku/Public/js/jquery_002.js"></script>
  
 
   <script>
@@ -617,8 +617,8 @@
     });
   </script>
 
-    <script src="/yuanku/Public/js/common.js"></script>
-  <script src="/yuanku/Public/js/demo-switch.js">  </script>
+    <script src="/lidong/yuanku/Public/js/common.js"></script>
+  <script src="/lidong/yuanku/Public/js/demo-switch.js">  </script>
     <script>
     if($(window).width() >= 1200){
       $("body").addClass("boxed-layout");
--- Application/Runtime/Cache/Admin/ece27d07c030028619d58a2d06df29f7.php
+++ Application/Runtime/Cache/Admin/ece27d07c030028619d58a2d06df29f7.php
@@ -1,4 +1,4 @@
-<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<?php if (!defined('THINK_PATH')) exit();?>﻿ <!DOCTYPE html>
 <html lang="en"><head>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 	<title><?php echo ($title); ?>  后台管理</title>
@@ -370,87 +370,15 @@
         <div class="col-md-12">
           <div class="content-box">
              <div class="content"> 
-<link rel="stylesheet" href="/lidong/yuanku/Public/css/admin/news.css">
-<div class="panel panel-primary">
-	<div class="panel-heading">
-		<h5>新闻分类</h5>
-		<!--<button class="btn btn-success " onclick="location.href='<?php echo U('News/newsort');?>'">类型</button>
-		<button class="btn btn-success " onclick="location.href='<?php echo U('News/sortbyauthor');?>'">作者</button>
-		<button class="btn btn-success " onclick="location.href='<?php echo U('News/sortbytime');?>'">时间</button>-->
-		<button class="btn btn-success change" >类型</button>
-		<button class="btn btn-success change">作者</button>
-		<button class="btn btn-success change">时间</button>
 
-
-
-	</div>
-	<table class="table table-hover">
-      	<thead>
-        	<tr>
-	          	<th>#</th>
-	          	<th>ID</th>
-		        <th>新闻标题</th>
-		        <th>作者</th>
-		        <th>分类</th>
-		        <th>内容</th>
-		        <th>图片</th>
-		        <th>日期</th>
-		        <th class="text-center">操作</th>
-        	</tr>
-      	</thead>
-      	<tbody id="ajax_rt_box">
-      		<?php if(is_array($news_list)): $k = 0; $__LIST__ = $news_list;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($k % 2 );++$k;?><tr>
-      				<td><?php echo ($k); ?></td>
-					<td><?php echo ($vo['nid']); ?></td>
-					<td><?php echo ($vo['title']); ?></td>
-					<td><?php echo ($vo['name']); ?></td>
-					<td><?php echo ($vo['sort_name']); ?></td>
-					<td><?php echo ($vo['content']); ?></td>
-					<td><?php echo ($vo['img']); ?></td>
-					<td><?php echo ($vo['date']); ?></td>
-					<td class="text-center">
-						<div class="btn-group">
-							<button type="button" class="btn btn-success btn-xs"><span class="glyphicon glyphicon-pencil"></span></button>
-							<button type="button" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-trash"></span></button>
-						</div>
-					</td>
-				</tr><?php endforeach; endif; else: echo "" ;endif; ?>
-      	</tbody>
-    </table>
-</div>
-<div class="btn-toolbar text-center" role="toolbar">
-	<div class="btn-group">
-		<?php echo ($page_html); ?>
-	</div>
-</div>
-<script type="text/javascript">
-var ajax_url='<?php echo U("News/newsort");?>';
-$(function(){
-	var arr=$(".change");
-	$.each(arr,function(i,item){
-        $(item).click(function(){
-        
-        	value=i;
-//      	$.post(ajax_url,{"value":value});
-        	$.ajax({
-			   type: "POST",
-			   url: "<?php echo U("News/newsort_j");?>",
-			   data: {"value":value},
-			   success: function(msg){
-				$("#ajax_rt_box").html(msg)
-     
-   }
-});
-        })
-	
-	})
-
-	
-})
-
-	
-</script>
-
+ <div>
+ 	添加新闻
+ 	<form action="">
+ 		
+ 		新闻名称：<input type="text">
+ 		<input type="submit" class="btn btn-success" value="添加"> 
+ 	</form>
+ </div>
 
  
  </div>
