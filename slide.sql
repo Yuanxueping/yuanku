c22bdcb8f2125f94b73688a2272b35b48c1c08a1
huanhuanya <923413786@qq.com>
2016-10-30 00:04:56 +0800

    重新上传了一份slide表，大家可以在数据库中删除并重新导入

--- slide.sql
+++ slide.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2016 年 10 月 28 日 01:38
+-- 生成日期: 2016 年 10 月 29 日 16:02
 -- 服务器版本: 5.5.20
 -- PHP 版本: 5.3.10
 
@@ -28,18 +28,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `slide` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
   `img` varchar(255) NOT NULL,
   `date` varchar(255) NOT NULL,
-  `content` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=14 ;
 
 --
 -- 转存表中的数据 `slide`
 --
 
-INSERT INTO `slide` (`id`, `img`, `date`, `content`) VALUES
-(1, '', '1477617856', '');
+INSERT INTO `slide` (`id`, `name`, `img`, `date`) VALUES
+(11, 'lunbo_1', 'img/lunbo/5812fb2889eff.jpg', '1477638952'),
+(12, 'lunbo_2', 'img/lunbo/5812fb6c86227.jpg', '1477639020'),
+(13, 'lunbo_3', 'img/lunbo/5812fb824dc55.jpg', '1477639042');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
